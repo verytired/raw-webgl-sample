f5800eb593e091c00b95df4e4ea03c9e80be1720
sano-yutaka <sano.yutaka@gmail.com>
2016-05-13 00:11:53 +0900

    クリアする色を指定

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 
+window.onload = function () {
 
-//canvasへの参上を変数に取得する
-let c = document.getElementById('canvas');
+    //canvasへの参上を変数に取得する
+    let c = document.getElementById('canvas');
 
-//WebGLコンテキストをcanvasから取得する
-let gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+    //WebGLコンテキストをcanvasから取得する
+    let gl = c.getContext('webgl') || c.getContext('experimental-webgl');
 
-//WebGLコンテキストの取得ができたかどうか
-if(gl){
-  console.log('supports webgl');
-}else{
-  console.log('webgl not supported');
-}
+    //WebGLコンテキストの取得ができたかどうか
+    if (gl) {
+        console.log('supports webgl');
+    } else {
+        console.log('webgl not supported');
+    }
+
+    // クリアする色を指定
+    gl.clearColor(0, 0, 0, 1.0);
+
+    //　エレメントをクリア
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+};
