697c79d1ba6a9c6261acf38636ed1fa407ccfea4
Yutaka Sano <sano_yutaka_gn@cyberagent.co.jp>
2016-05-16 17:40:00 +0900

    アニメーション追加

--- src/Sample2.js
+++ src/Sample2.js
@@ -4,170 +4,205 @@ import {matIV, qtnIV, torus, cube, hsva ,sphere} from "./minMatrix";
  */
 
 class Sample2 {
-    /**
-     * constructor
-     * コンストラクタ
-     */
-    constructor() {
+  /**
+   * constructor
+   * コンストラクタ
+   */
+  constructor() {
 
-        //canvasへの参上を変数に取得する
-        let c = document.getElementById('canvas');
-        // size指定
-        c.width = 512;
-        c.height = 512;
-        this.canvas = c;
+    //canvasへの参上を変数に取得する
+    let c = document.getElementById('canvas');
+    // size指定
+    c.width = 512;
+    c.height = 512;
+    this.canvas = c;
 
-        //WebGLコンテキストをcanvasから取得する
-        this.gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+    //WebGLコンテキストをcanvasから取得する
+    this.gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+
+    // 行列計算
+    this.mat = null;
+    // レンダリング用カウンタ
+    this.count = 0;
+  }
+
+  /**
+   * run
+   * サンプルコード実行
+   */
+  run() {
+
+    //WebGLコンテキストの取得ができたかどうか
+    if (this.gl) {
+      console.log('supports webgl');
+    } else {
+      console.log('webgl not supported');
+      return
     }
 
-    /**
-     * run
-     * サンプルコード実行
-     */
-    run() {
+    // クリアする色を指定
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-        //WebGLコンテキストの取得ができたかどうか
-        if (this.gl) {
-            console.log('supports webgl');
-        } else {
-            console.log('webgl not supported');
-            return
-        }
+    // エレメントをクリア
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 
-        // クリアする色を指定
-        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // 三角形を形成する頂点のデータを受け取る
+    this.triangleData = this.genTriangle();
 
-        // エレメントをクリア
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    // 頂点データからバッファを生成
+    let vertexBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.triangleData.p), this.gl.STATIC_DRAW);
 
-        // 三角形を形成する頂点のデータを受け取る
-        let triangleData = this.genTriangle();
+    // シェーダとプログラムオブジェクト
+    const vertexSource = document.getElementById('vs').textContent;
+    const fragmentSource = document.getElementById('fs').textContent;
 
-        // 頂点データからバッファを生成
-        let vertexBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(triangleData.p), this.gl.STATIC_DRAW);
+    // ユーザー定義のプログラムオブジェクト生成関数
+    this.programs = this.createShaderProgram(vertexSource, fragmentSource);
 
-        // シェーダとプログラムオブジェクト
-        const vertexSource = document.getElementById('vs').textContent;
-        const fragmentSource = document.getElementById('fs').textContent;
+    // プログラムオブジェクトに三角形の頂点データを登録
+    let attLocation = this.gl.getAttribLocation(this.programs, 'position');
+    this.gl.enableVertexAttribArray(attLocation);
+    this.gl.vertexAttribPointer(attLocation, 3, this.gl.FLOAT, false, 0, 0);
 
-        // ユーザー定義のプログラムオブジェクト生成関数
-        let programs = this.createShaderProgram(vertexSource, fragmentSource);
+    // 行列の初期化
+    this.mat = new matIV();
+    this.mMatrix = this.mat.identity(this.mat.create());
+    this.vMatrix = this.mat.identity(this.mat.create());
+    this.pMatrix = this.mat.identity(this.mat.create());
+    this.vpMatrix = this.mat.identity(this.mat.create());
+    this.mvpMatrix = this.mat.identity(this.mat.create());
 
-        // プログラムオブジェクトに三角形の頂点データを登録
-        let attLocation = this.gl.getAttribLocation(programs, 'position');
-        this.gl.enableVertexAttribArray(attLocation);
-        this.gl.vertexAttribPointer(attLocation, 3, this.gl.FLOAT, false, 0, 0);
+    // ビュー座標変換行列
+    let cameraPosition = [0.0, 0.0, 3.0]; // カメラの位置
+    let centerPoint = [0.0, 0.0, 0.0];    // 注視点
+    let cameraUp = [0.0, 1.0, 0.0];       // カメラの上方向
+    this.mat.lookAt(cameraPosition, centerPoint, cameraUp, this.vMatrix);
 
-        // 行列の初期化
-        let mat = new matIV();
-        let mMatrix = mat.identity(mat.create());
-        let vMatrix = mat.identity(mat.create());
-        let pMatrix = mat.identity(mat.create());
-        let vpMatrix = mat.identity(mat.create());
-        let mvpMatrix = mat.identity(mat.create());
+    // プロジェクションのための情報を揃える
+    let fovy = 45;                             // 視野角
+    let aspect = this.canvas.width / this.canvas.height; // アスペクト比
+    let near = 0.1;                            // 空間の最前面
+    let far = 10.0;                            // 空間の奥行き終端
+    this.mat.perspective(fovy, aspect, near, far, this.pMatrix);
 
-        // モデル座標変換行列
-        // 移動
-        let move = [0.5, 0.5, 0.0];           // 移動量はXYそれぞれ0.5
-        mat.translate(mMatrix, move, mMatrix);
+    // 行列を掛け合わせてVPマトリックスを生成しておく
+    this.mat.multiply(this.pMatrix, this.vMatrix, this.vpMatrix);   // pにvを掛ける
 
-        // 回転
-        let radians = 90 * Math.PI / 180;
-        let axis = [0.0, 0.0, 1.0];
-        mat.rotate(mMatrix, radians, axis, mMatrix);
+    // rendering開始
+    this.render();
+  }
 
-        // ビュー座標変換行列
-        let cameraPosition = [0.0, 0.0, 3.0]; // カメラの位置
-        let centerPoint = [0.0, 0.0, 0.0];    // 注視点
-        let cameraUp = [0.0, 1.0, 0.0];       // カメラの上方向
-        mat.lookAt(cameraPosition, centerPoint, cameraUp, vMatrix);
+  /**
+   * レンダリング関数の定義
+   */
+  render() {
 
-        // プロジェクションのための情報を揃える
-        let fovy = 45;                             // 視野角
-        let aspect = this.canvas.width / this.canvas.height; // アスペクト比
-        let near = 0.1;                            // 空間の最前面
-        let far = 10.0;                            // 空間の奥行き終端
-        mat.perspective(fovy, aspect, near, far, pMatrix);
+    // Canvasエレメントをクリアする
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 
-        // 行列を掛け合わせてMVPマトリックスを生成
-        mat.multiply(pMatrix, vMatrix, vpMatrix);   // pにvを掛ける
-        mat.multiply(vpMatrix, mMatrix, mvpMatrix); // さらにmを掛ける
+    // モデル座標変換行列を一度初期化してリセットする
+    this.mat.identity(this.mMatrix);
 
-        // シェーダに行列を送信する
-        let uniLocation = this.gl.getUniformLocation(programs, 'mvpMatrix');
-        this.gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    // カウンタをインクリメントする
+    this.count++;
 
-        // 描画
-        this.gl.drawArrays(this.gl.TRIANGLES, 0, triangleData.p.length / 3);
-        this.gl.flush();
+    // モデル座標変換行列
+    // 移動
+    let move = [0.5, 0.5, 0.0];           // 移動量はXYそれぞれ0.5
+    this.mat.translate(this.mMatrix, move, this.mMatrix);
+
+    // 回転
+    let radians = (this.count % 360) * Math.PI / 180;
+    let axis = [0.0, 0.0, 1.0];
+    this.mat.rotate(this.mMatrix, radians, axis, this.mMatrix);
+
+
+    // 行列を掛け合わせてMVPマトリックスを生成
+    this.mat.multiply(this.pMatrix, this.vMatrix, this.vpMatrix);   // pにvを掛ける
+    this.mat.multiply(this.vpMatrix, this.mMatrix, this.mvpMatrix); // さらにmを掛ける
+
+    // シェーダに行列を送信する
+    let uniLocation = this.gl.getUniformLocation(this.programs, 'mvpMatrix');
+    this.gl.uniformMatrix4fv(uniLocation, false, this.mvpMatrix);
+
+    // VPマトリックスにモデル座標変換行列を掛ける
+    this.mat.multiply(this.vpMatrix, this.mMatrix, this.mvpMatrix);
+
+    // 描画
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, this.triangleData.p.length / 3);
+    this.gl.flush();
+
+    // 再帰呼び出し
+    requestAnimationFrame(()=> {
+      this.render();
+    });
+  }
+
+  /**
+   * createShaderProgram
+   * プログラムオブジェクト生成関数
+   */
+  createShaderProgram(vertexSource, fragmentSource) {
+
+    // シェーダオブジェクトの生成
+    let vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+    let fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+
+    // シェーダにソースを割り当ててコンパイル
+    this.gl.shaderSource(vertexShader, vertexSource);
+    this.gl.compileShader(vertexShader);
+    this.gl.shaderSource(fragmentShader, fragmentSource);
+    this.gl.compileShader(fragmentShader);
+
+    // シェーダーコンパイルのエラー判定
+    if (this.gl.getShaderParameter(vertexShader, this.gl.COMPILE_STATUS)
+      && this.gl.getShaderParameter(fragmentShader, this.gl.COMPILE_STATUS)) {
+      console.log('Success Shader Compile');
+    } else {
+      console.log('Faild Shader Compile');
+      console.log('vertexShader', this.gl.getShaderInfoLog(vertexShader));
+      console.log('fragmentShader', this.gl.getShaderInfoLog(fragmentShader));
     }
 
-    /**
-     * createShaderProgram
-     * プログラムオブジェクト生成関数
-     */
-    createShaderProgram(vertexSource, fragmentSource) {
+    // プログラムオブジェクトの生成から選択まで
+    const programs = this.gl.createProgram();
 
-        // シェーダオブジェクトの生成
-        let vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
-        let fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+    this.gl.attachShader(programs, vertexShader);
+    this.gl.attachShader(programs, fragmentShader);
+    this.gl.linkProgram(programs);
 
-        // シェーダにソースを割り当ててコンパイル
-        this.gl.shaderSource(vertexShader, vertexSource);
-        this.gl.compileShader(vertexShader);
-        this.gl.shaderSource(fragmentShader, fragmentSource);
-        this.gl.compileShader(fragmentShader);
-
-        // シェーダーコンパイルのエラー判定
-        if (this.gl.getShaderParameter(vertexShader, this.gl.COMPILE_STATUS)
-            && this.gl.getShaderParameter(fragmentShader, this.gl.COMPILE_STATUS)) {
-            console.log('Success Shader Compile');
-        } else {
-            console.log('Faild Shader Compile');
-            console.log('vertexShader', this.gl.getShaderInfoLog(vertexShader));
-            console.log('fragmentShader', this.gl.getShaderInfoLog(fragmentShader));
-        }
-
-        // プログラムオブジェクトの生成から選択まで
-        const programs = this.gl.createProgram();
-
-        this.gl.attachShader(programs, vertexShader);
-        this.gl.attachShader(programs, fragmentShader);
-        this.gl.linkProgram(programs);
-
-        // プログラムオブジェクトのエラー判定処理
-        if (this.gl.getProgramParameter(programs, this.gl.LINK_STATUS)) {
-            this.gl.useProgram(programs);
-        } else {
-            console.log('Failed Link Program', this.gl.getProgramInfoLog(programs));
-        }
-
-        // 生成したプログラムオブジェクトを戻り値として返す
-        return programs;
+    // プログラムオブジェクトのエラー判定処理
+    if (this.gl.getProgramParameter(programs, this.gl.LINK_STATUS)) {
+      this.gl.useProgram(programs);
+    } else {
+      console.log('Failed Link Program', this.gl.getProgramInfoLog(programs));
     }
 
-    /**
-     * genTriangle
-     * 三角形の頂点情報を返却する
-     */
-    genTriangle() {
-        let obj = {};
-        obj.p = [
-            // ひとつ目の三角形
-            0.0, 0.5, 0.0,
-            0.5, -0.5, 0.0,
-            -0.5, -0.5, 0.0,
+    // 生成したプログラムオブジェクトを戻り値として返す
+    return programs;
+  }
 
-            // ふたつ目の三角形
-            0.0, -0.5, 0.0,
-            0.5, 0.5, 0.0,
-            -0.5, 0.5, 0.0
-        ];
-        return obj;
-    }
+  /**
+   * genTriangle
+   * 三角形の頂点情報を返却する
+   */
+  genTriangle() {
+    let obj = {};
+    obj.p = [
+      // ひとつ目の三角形
+      0.0, 0.5, 0.0,
+      0.5, -0.5, 0.0,
+      -0.5, -0.5, 0.0,
+
+      // ふたつ目の三角形
+      0.0, -0.5, 0.0,
+      0.5, 0.5, 0.0,
+      -0.5, 0.5, 0.0
+    ];
+    return obj;
+  }
 }
 
 module.exports = Sample2;
